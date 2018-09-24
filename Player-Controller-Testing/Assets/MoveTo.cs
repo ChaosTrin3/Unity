22a40969c295d5fe9255b419875f4b8b29b00dd0
JoshT <jdt015@bucknell.edu>
2018-09-24 19:11:54 -0400

    Added NavMesh.asset and wrote a basic MoveTo script which calls a PlayerMotor script to handle motion.

---
+++ Player-Controller-Testing/Assets/MoveTo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class MoveTo : MonoBehaviour {
+
+    Camera cam;
+    PlayerMotor motor;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                motor.MoveToTarget(hit.point);
+            }
+        }
+
+    }
+}
---
+++ Player-Controller-Testing/Assets/PlayerMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent.GetComponent<NavMeshAgent>();
+	}
+    public void MoveToTarget(Vector3 target)
+    {
+        agent.SetDestination(target);
+    }
+}
